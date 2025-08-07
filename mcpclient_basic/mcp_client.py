6e97d986ef4da17aa905fb9d7ac739fbba9f194f
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-08-07 21:35:37 +0500

    Update mcp_client.py

--- mcpclient_basic/mcp_client.py
+++ mcpclient_basic/mcp_client.py
@@ -1,3 +1,6 @@
 from mcp.client.streamable_http import streamablehttp_client
 from mcp.client.session import ClientSession
+import rich
 
+async def main():
+    async with streamablehttp_client(url="http://localhost:8000")
