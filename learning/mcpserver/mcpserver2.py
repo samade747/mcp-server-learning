9b1acce24a3eb2f5c64590edacd2bb9b79206e40
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-09-30 10:34:35 +0500

    1034AM 30 9 2025

--- learning/mcpserver/mcpserver2.py
+++
@@ -1,4 +0,0 @@
-import asyncio
-from mcp.client.streamable_http import StreamableHTTPClient
-from mcp.server.fastmcp import  ClientSession
-
--- mcpclient_basic/mcp_client.py
+++
@@ -1,6 +0,0 @@
-from mcp.client.streamable_http import streamablehttp_client
-from mcp.client.session import ClientSession
-import rich
-
-async def main():
-    async with streamablehttp_client(url="http://localhost:8000")
